bf8c8f7500697f62bdab5cb6e03d534140abb0e9
Eternal-Ben <Eternalst@live.fr>
2018-10-02 21:14:21 +0200

    Creation : class Price Motif : afin de prevoir une methode differente pour le calcul de chaque reduction dans le futur. A faire : realiser les methodes de retour

---
+++ BoGoVoyage/Tools/Price.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoGoVoyage.Tools
+{
+    public class Price
+    {
+        private int Discount_60()
+        {
+            if (true)
+            {
+                return Discount_60;
+
+            }
+
+        }
+
+        private int Discount_30()
+        {
+            if (true)
+            {
+                return Discount_30;
+
+            }
+
+        }
+
+        private int Discount_25()
+        {
+            if (true)
+            {
+
+                return Discount_25;
+            }
+        }
+    }
+}
